bda92999546391f774cd483faa697bc02195bbc8
sergeyshishkin <sergejshishkin@gmail.com>
2018-08-19 02:38:38 +0300

    improve calculating damage and health

--- game-scratch.py
+++ game-scratch.py
@@ -2,87 +2,36 @@ from sys import exit
 from random import randint
 import time
 
-## WEATHER CONDITIONS SETTER, THAT DEFINES PROPER ITEM IN INVADING SCENE
-# weather_dict = {'turn on':'storm','keep off':'heat', 'turn favorite':'cloudy'}
-# print("""Friend of yours gave you a weather generator few yers ago. Wanna change the weather?
-# Here are 3 options: turn on, keep off, turn favorite""")
-#
-# weather_conditions = ''
-#
-# while weather_conditions != None:
-#     user_choice = input("> ")
-#
-#     if user_choice in weather_dict:
-#         weather_conditions = weather_dict[user_choice]
-#         print(weather_conditions)
-#         exit(0)
-#     else:
-#         print("No such mode, try one more time")
-
-
 ## HEALTH/DAMAGE MANAGMENT
 
-health = 800 # updatable health level during battle
-damage = 0 # variable storing damage level
+# health = 800 # updatable health level during battle
+# damage = 0 # variable storing damage level
+#
+# while health >= 0:
+#     damage = randint(50, 150)
+#     health -= damage
+# ## MESSAGING DURING BATTLE
+#     if damage >= 50 and damage <= 70:
+#         print("Shaking hands, huh? Damage is just",damage)
+#         #print("New health is ", health)
+#     elif damage >= 130 and damage <= 150:
+#         print("What a hit! Damage is",damage)
+#         #print("New health is ", health)
+#     else:
+#         print("Damage is",damage)
+#         #print("New health is ", health)
+#
+#     if health <= 0:
+#         print("Well, looks like somebody is dead. Farewell, buddy")
+#         break
+#     time.sleep(1)
+hero_health = 100
 
-while health >= 0:
-    damage = randint(50, 150)
-    health -= damage
-## MESSAGING DURING BATTLE
-    if damage >= 50 and damage <= 70:
-        print("Shaking hands, huh? Damage is just",damage)
-        #print("New health is ", health)
-    elif damage >= 130 and damage <= 150:
-        print("What a hit! Damage is",damage)
-        #print("New health is ", health)
-    else:
-        print("Damage is",damage)
-        #print("New health is ", health)
+def make_damage(health):
+    damage = randint(1, 2) # GENERATE DAMAGE
+    print("Damage is", damage)
+    health -= damage # SUBTRACT DAMAGE FROM HEALTH
+    print("Current health is", health)
+    return health
 
-    if health <= 0:
-        print("Well, looks like somebody is dead. Farewell, buddy")
-        break
-    time.sleep(1)
-
-## INVADING A BASE
-
-# # If the weather is heat
-# heat = """Here is a base. Guard stands at the door. It's too hot. You understand
-#  that you can trick guard, to slip to the base.
-# As remember, you have 'items'. What you can offer, to distract his attention?"""
-#
-#
-# #if the weather is stormy
-# storm = """Here is a base. Guard stands at the door. It's very windy, almost
-# stormy. You understand that you can trick guard, to slip to the base.
-# As remember, you have 'items'. What you can offer, to distract his attention?"""
-#
-#
-# # if te weather is Cloudy
-# cloudy = """Here is a base. Guard stands at the door. It's cloudy, and
-# melancholic weather. You understand that you can trick guard, to slip to the base.
-# As remember, you have 'items'. What you can offer, to distract his attention?"""
-#
-#
-# items = ['gun', 'cigarettes', 'phone', 'hat']
-# weather_conditions = 'cloudy'
-#
-#
-# item_choice = input(">_> ")
-# ## # IDEA: REFACTOR THIS BY STORING OPTIONS IN DICTIONARY
-# if item_choice == 'gun':
-#     print('start battle')
-# elif weather_conditions == 'heat' and item_choice != 'hat':
-#     print("You fail")
-# elif weather_conditions == 'heat' and item_choice == 'hat':
-#     print("You pass")
-# elif weather_conditions == 'storm' and item_choice != 'phone':
-#     print("You fail")
-# elif weather_conditions == 'storm' and item_choice == 'phone':
-#     print("You pass")
-# elif weather_conditions == 'cloudy' and item_choice != 'cigarettes':
-#     print("You fail")
-# elif weather_conditions == 'cloudy' and item_choice == 'cigarettes':
-#     print("You pass")
-# else:
-#     print("Hold on! You have only 4 items. Try one more time")
+make_damage(hero_health)
--- game.py
+++ game.py
@@ -3,74 +3,34 @@ from random import randint
 import time
 
 
-# class BattleScene(object):
-#     def battle(self):
-#         health = 800 # updatable health level during battle
-#         damage = 0 # variable storing damage level
+# ### BRANCHING SCENARIO IN START SCENE
+# weather_dict = {'turn on':'storm','keep off':'heat', 'turn favorite':'cloudy'}
+# item_dict = {'storm': 'phone', 'heat': 'cap', 'cloudy':'cigarrete'}
 #
-#         while health >= 0:
-#             damage = randint(50, 150)
-#             health -= damage
-#         ## MESSAGING DURING BATTLE
-#             if damage >= 50 and damage <= 70:
-#                 print("Shaking hands, huh? Damage is just",damage)
-#                 #print("New health is ", health)
-#             elif damage >= 130 and damage <= 150:
-#                 print("What a hit! Damage is",damage)
-#                 #print("New health is ", health)
-#             else:
-#                 print("Damage is",damage)
-#                 #print("New health is ", health)
+# weather_choice = input('Choose weather mode> ')
 #
-#             if health <= 0:
-#                 print("Well, looks like somebody is dead. Farewell, buddy")
-#                 break
-#             time.sleep(1)
-# a = BattleScene()
-# a.battle()
-
-weather_dict = {'turn on':'storm','keep off':'heat', 'turn favorite':'cloudy'}
-item_dict = {'storm': 'phone', 'heat': 'cap', 'cloudy':'cigarrete'}
-
-weather_choice = input('Choose weather > ')
-
-## DEFINE WEATHER
-if weather_choice in weather_dict:
-    item_key = weather_dict[weather_choice]
-    print(item_key)
-else:
-    print("None")
-
-## DEFINE PROPER ITEM
-if item_key in item_dict:
-    item_val = item_dict[item_key]
-    print(item_val)
-else:
-    print("None")
-
-next_step = input("Choose item > ")
-
-## DEFINE PASS OR FAIL
-if next_step == item_val:
-    print("Go further")
-    print(next_step, item_val)
-elif next_step == 'gun':
-    print("Start battle")
-else:
-    print("You fail")
-    print(next_step, item_val)
-
-        # print("""Friend of yours gave you a weather generator few yers ago. Wanna change the weather?
-        # Here are 3 options: turn on, keep off, turn favorite""")
-        #
-        # weather_conditions = ''
-        #
-        # while weather_conditions != None:
-        #     user_choice = input("> ")
-        #
-        #     if user_choice in weather_dict:
-        #         weather_conditions = weather_dict[user_choice]
-        #         return weather_conditions
-        #         exit(0)
-        #     else:
-        #         print("No such mode, try one more time")
+# ## DEFINE WEATHER
+# if weather_choice in weather_dict:
+#     item_key = weather_dict[weather_choice]
+#     print(item_key)
+# else:
+#     print("None")
+#
+# ## DEFINE PROPER ITEM
+# if item_key in item_dict:
+#     item_val = item_dict[item_key]
+#     print(item_val)
+# else:
+#     print("None")
+#
+#
+# ### DEFINE PASS OR FAIL IN INVADING SCENE
+# next_step = input("Choose item > ")
+# if next_step == item_val:
+#     print("Go further")
+#     print(next_step, item_val)
+# elif next_step == 'gun':
+#     print("Start battle")
+# else:
+#     print("You fail")
+#     print(next_step, item_val)
