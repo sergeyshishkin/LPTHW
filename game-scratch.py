01e5dab2033147384bac8c766e1d1957d76e6a5d
sergeyshishkin <sergejshishkin@gmail.com>
2018-08-23 00:55:34 +0300

    prototype of bedroom to invade to battle scene transition

--- game-scratch.py
+++ game-scratch.py
@@ -3,30 +3,128 @@ from random import randint
 import time
 
 
-# ### PROTOTYPE OF HEALTH|DAMAGE MECHANIZM
+### PROTOTYPE OF BEDROOM TO INVADE SCENE TRANSITION
+### A = CLASSNAME().FUNCNAME()
 
-# a = 40
-# b = 20
+class Bedroom():
+    def enter(self):
+        print("Launch Bedroom")
+        global o
+        o = input("Choose item> ")
+        i = input("Next scene> ")
+        if i == "1":
+            next_scene = Invade().enter()
+        else:
+            print("DEAD END")
+            exit(1)
+
+class Invade():
+    def enter(self):
+        print("Launch Invade")
+        v = input("Choose your item> ")
+        if v == o:
+            print("MATCH")
+        elif v == '2':
+            next_scene = Battle().enter()
+        else:
+            print("NOPE")
+            exit(1)
+
+
+class Battle():
+    def enter(self):
+        hero_health = 200
+        enemy_health = 200
+
+        def fun(hero_health, enemy_health):
+
+            while hero_health > 0 and enemy_health > 0:
+
+                # FUNCTION THAT GENERATES DAMAGE, SHOW TEXT ABOUT DAMAGE AND WHO DID THE DAMAGE
+                def generate_damage(user):
+                    damage = randint(50, 150)
+                    if damage >= 50 and damage <= 70:
+                        print(user, "strikes poorly and hit only",damage,"damage")
+                    elif damage >= 130 and damage <= 150:
+                        print(user, "strikes a great hit! Damage is",damage)
+                    else:
+                        print(user, "strikes ordinary",damage,"damage")
+                    return damage
+
+                # CALLING generate_damage FUNCTION
+                damage = generate_damage("Enemy")
+
+                # HERE IS CALCULATING HEALTH AND FATAL LEVEL FOR HERO
+                hero_health -= damage
+                print ("New hero health is: ", hero_health) #IN PRODUCTION, MOVE THIS LINE AFTER "ELSE: PASS"
+                if hero_health <= 0:
+                    print("Well, looks like hero is dead. Farewell, buddy")
+                    print("Luckly, you are in the game, it's a fiction. You can try kick his ass again")
+                    keys = input("Press Enter to continue...")
+                    next_scene = Battle().enter()
+                    break
+
+                else:
+                    pass
+
+                time.sleep(1) #DELAY TO MAKE GAME NOT SO RAPID
+
+                # CALLING generate_damage FUNCTION
+                damage = generate_damage("Hero")
+
+                # HERE IS CALCULATING HEALTH AND FATAL LEVEL FOR ENEMY
+                enemy_health -= damage
+                print ("New enemy health is: ", enemy_health) #IN PRODUCTION, MOVE THIS LINE AFTER "ELSE: PASS"
+                if enemy_health <= 0:
+                    print("Enemy is dead. Hero goes further")
+                    break
+                else:
+                    pass
+
+                time.sleep(1) #DELAY TO MAKE GAME NOT SO RAPID
+
+        fun(hero_health, enemy_health)
+
+class Start():
+
+    def __init__(self, scene):
+        self.scene = scene
+        scene = Bedroom().enter()
+
+a = Start('bedroom')
+
+
+
+
+# ### PROTOTYPE OF BEDROOM TO INVADE SCENE TRANSITION
+# ### A = CLASSNAME().FUNCNAME()
 #
-# def fun(a,b):
-#     def text():
-#         damage = randint(5, 7)
-#         if damage == 5:
-#             print("damage is 5")
-#         elif damage == 6:
-#             print("damage is 6")
+# class Bedroom():
+#     def enter(self):
+#         print("Launch Bedroom")
+#         global o
+#         o = input("Choose item> ")
+#         i = input("Next scene> ")
+#         if i == "1":
+#             next_scene = Invade().enter()
 #         else:
-#             print("damage is 7")
-#         return damage
+#             print("DEAD END")
+#             exit(1)
 #
-#     while a > 0 and b > 0:
-#         damage = text()
-#         a -= damage
-#         print("New a:", a)
-#         damage = text()
-#         b -= damage
-#         print("New b:", b)
-#         #print ("loc_a: ", a, "loc_b: ", b)
-#         if a <= 0 or b <= 0:
-#             break
-# fun(a,b)
+# class Invade():
+#     def enter(self):
+#         print("Launch Invade")
+#         v = input("Choose your item> ")
+#         if v == o:
+#             print("MATCH")
+#         else:
+#             print("NOPE")
+#             exit(1)
+#
+# class Start():
+#
+#     def __init__(self, scene):
+#         self.scene = scene
+#         scene = Bedroom().enter()
+#
+# a = Start('bedroom')
--- game.py
+++ game.py
@@ -3,39 +3,39 @@ from random import randint
 import time
 
 
-# ### BRANCHING SCENARIO IN START SCENE
-# weather_dict = {'turn on':'storm','keep off':'heat', 'turn favorite':'cloudy'}
-# item_dict = {'storm': 'phone', 'heat': 'cap', 'cloudy':'cigarrete'}
-#
-# weather_choice = input('Choose weather mode> ')
-#
-# ## DEFINE WEATHER
-# if weather_choice in weather_dict:
-#     item_key = weather_dict[weather_choice]
-#     print(item_key)
-# else:
-#     print("None")
-#
-# ## DEFINE PROPER ITEM
-# if item_key in item_dict:
-#     item_val = item_dict[item_key]
-#     print(item_val)
-# else:
-#     print("None")
-#
-#
-# ### DEFINE PASS OR FAIL IN INVADING SCENE
-# next_step = input("Choose item > ")
-# if next_step == item_val:
-#     print("Go further")
-#     print(next_step, item_val)
-# elif next_step == 'gun':
-#     print("Start battle")
-# else:
-#     print("You fail")
-#     print(next_step, item_val)
+### BRANCHING SCENARIO IN START SCENE
+weather_dict = {'turn on':'storm','keep off':'heat', 'turn favorite':'cloudy'}
+item_dict = {'storm': 'phone', 'heat': 'cap', 'cloudy':'cigarrete'}
 
-# ### COMPLETED HEALTH|DAMAGE MECHANISM
+weather_choice = input('Choose weather mode> ')
+
+## DEFINE WEATHER
+if weather_choice in weather_dict:
+    item_key = weather_dict[weather_choice]
+    print(item_key)
+else:
+    print("None")
+
+## DEFINE PROPER ITEM
+if item_key in item_dict:
+    item_val = item_dict[item_key]
+    print(item_val)
+else:
+    print("None")
+
+
+### DEFINE PASS OR FAIL IN INVADING SCENE
+next_step = input("Choose item > ")
+if next_step == item_val:
+    print("Go further")
+    print(next_step, item_val)
+elif next_step == 'gun':
+    print("Start battle")
+else:
+    print("You fail")
+    print(next_step, item_val)
+
+### COMPLETED HEALTH|DAMAGE MECHANISM
 hero_health = 200
 enemy_health = 200
 
