a2a937d3fab892021a6a36414c330cc3fbbad057
sergeyshishkin <sergejshishkin@gmail.com>
2018-08-24 22:42:15 +0300

    + complete skeleton of StartScene\Bedroom, Battle, Invade + add BaseAttack scene class + add exits in Battle Scene + pass a weather pick to Invade scene + add exit to BaseAttack scene after Invade success

--- game-scratch.py
+++ game-scratch.py
@@ -3,28 +3,45 @@ from random import randint
 import time
 
 
-### PROTOTYPE OF BEDROOM TO INVADE SCENE TRANSITION
+### PROTOTYPE OF BEDROOM TO INVADE TO BATTLE SCENE TRANSITION
 ### A = CLASSNAME().FUNCNAME()
 
 class Bedroom():
     def enter(self):
-        print("Launch Bedroom")
-        global o
-        o = input("Choose item> ")
-        i = input("Next scene> ")
-        if i == "1":
+        weather_dict = {'turn on':'storm','keep off':'heat', 'favorite':'cloudy'}
+        item_dict = {'storm': 'phone', 'heat': 'cap', 'cloudy':'cigarrete'}
+
+        print("You can choose turn on, keep off and favorite")
+        weather_choice = input('Choose weather mode> ')
+
+        global item_key
+        ## DEFINE WEATHER
+        if weather_choice in weather_dict:
+            item_key = weather_dict[weather_choice]
+            print(item_key) #for debug
+        else:
+            print("None")
+
+        global item_val
+        ## DEFINE PROPER ITEM
+        if item_key in item_dict:
+            item_val = item_dict[item_key]
+            print(item_val) #for debug
             next_scene = Invade().enter()
         else:
-            print("DEAD END")
-            exit(1)
+            print("None")
 
 class Invade():
     def enter(self):
+        user_item_list = ['phone','cap','cigarrete', 'gun']
         print("Launch Invade")
+        print("You choice generates",item_key,"weather")
+        print("You have",user_item_list)
         v = input("Choose your item> ")
-        if v == o:
-            print("MATCH")
-        elif v == '2':
+        if v == item_val:
+            print("MATCH, you enter a base")
+            next_scene = BaseAttack().enter()
+        elif v == 'gun':
             next_scene = Battle().enter()
         else:
             print("NOPE")
@@ -33,6 +50,7 @@ class Invade():
 
 class Battle():
     def enter(self):
+        print("There will be BLOOD!")
         hero_health = 200
         enemy_health = 200
 
@@ -76,14 +94,19 @@ class Battle():
                 enemy_health -= damage
                 print ("New enemy health is: ", enemy_health) #IN PRODUCTION, MOVE THIS LINE AFTER "ELSE: PASS"
                 if enemy_health <= 0:
-                    print("Enemy is dead. Hero goes further")
-                    break
+                    print("Enemy is dead. But you nake a lot of noise. Other guards shoot you down. Restart a Invade scene")
+                    next_scene = Invade().enter()
                 else:
                     pass
 
                 time.sleep(1) #DELAY TO MAKE GAME NOT SO RAPID
 
         fun(hero_health, enemy_health)
+
+class BaseAttack():
+    def enter(self):
+        print("You enter a Base, good job")
+        exit(0)
 
 class Start():
 
